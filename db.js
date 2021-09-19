756b2c90269c40185605c95d4757dc79dfdba6a9
ipankajchaudhary <65165139+ipankajchaudhary@users.noreply.github.com>
2021-09-19 22:09:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const mongoURI = "mongodb+srv://Pankaj:Pankaj2002@cluster0.uehfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://Pankaj:{shshsh}2@cluster0.uehfh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
